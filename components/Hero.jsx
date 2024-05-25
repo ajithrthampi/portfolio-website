af7c050420c51b32cee3f9b6718ecca7fa8ae6c3
phozpho <ajithrthampi999@gmail.com>
2024-05-25 19:37:25 +0530

    build error solve

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
     <div id="home" className="  h-screen flex justify-center items-center flex-col   px-10">
       <div className='flex justify-center items-center flex-col gap-3'>
         <img className='w-32 h-32 rounded-full object-cover' src={profile.src} alt="" />
-        <p className='text-white text-sm'>Hi i&apos;am Ajith</p>
+        <p className='text-white text-sm'>Hi i&apos;am Ajith.</p>
       </div>
 
       <TextGenerateEffect
